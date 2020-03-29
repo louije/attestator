30a6d2ea8eb91e32fcc3af9d62a69d6f3a9b3636
Louis-Jean Teitelbaum <ljt@meidosem.com>
2020-03-29 19:59:38 +0200

    Unbug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 const people = {
-  cathy: { name: "Catherine Guesde", birthdate: "13/01/1984", birthplace: "High-Wycombe (GB)" },
+  cathy:  { name: "Catherine Guesde", birthdate: "13/01/1984", birthplace: "High-Wycombe (GB)" },
   lucile: { name: "Lucile Sourdès", birthdate: "24/02/1984", birthplace: "Guingamp" },
   barney: { name: "Barney Cohen", birthdate: "27/06/1986", birthplace: "Saintes" },
   louije: { name: "Louis-Jean Teitelbaum", birthdate: "27/05/1982", birthplace: "Maisons-Laffitte" }
@@ -16,9 +16,11 @@ app.use(express.static("public"));
 
 app.get("/print", (req, res) => {
   let { nickname, reasons } = req.query;
-  let { name, birthdate, birthplace } = people[nickname];
+  let { name, birthdate, birthplace } = people[nickname]
+      || { name: undefined, birthday: undefined, birthplace: undefined };
+
   print(name, birthdate, birthplace, reasons);
   res.sendFile(path.join(__dirname) + "/public/ok.html")
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Serving on http://localhost:${port}`));
